a03fbf86c922578aa0e71319e26d5e014ac118f0
Rick Jason <ken@vagrant.rserver>
2019-09-30 20:29:10 +0900

    init

--- test/fixtures/files/etc-profile.d-local.sh
+++ test/fixtures/files/etc-profile.d-local.sh
@@ -1,6 +1,3 @@
 alias vi=vim
 alias trr="tree -L 1 "
-alias lss="ls -la "
-alias relogin='exec $SHELL -l'
-alias vicolor_reset='echo -e "\e[m"'
-# alias vis='clear && tree ~/.vim/session'
+alias lss="tree -FaI 'node_modules|.git|.DS_Store'"
